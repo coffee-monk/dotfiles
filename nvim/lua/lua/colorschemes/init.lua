575fd73169779cff0b81190d0f3a4cfc52978486
coffee-monk <coffeemonk@hotmail.com>
2022-08-18 18:07:35 -0400

    add complete lua config

---
+++ nvim/lua/lua/colorschemes/init.lua
@@ -0,0 +1,59 @@
+-- Colorschemes >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+local themes = {
+	{ "Nightfox", "nightfox" },
+	{ "Nordfox", "nordfox" },
+	{ "Terafox", "terafox" },
+	{ "Tokyonight", "tokyonight" },
+	{ "Monokai", "monokai_pro" },
+	{ "Boo", "boo" },
+	{ "Jellybeans", "jellybeans-nvim" },
+	{ "Sonokai", "sonokai" },
+	{ "Solarized", "solarized" },
+
+	{ "Nord", { theme = "nord", lua_loc = "lualine.themes.nord" } },
+	{ "Dark", { theme = "vscode", lua_loc = "lualine.themes.vscode" } },
+	{ "Moonlight", { theme = "moonlight", lua_loc = "lualine.themes.moonlight" } },
+	{ "Melange", { theme = "melange", lua_loc = "lualine.themes.gruvbox" } },
+	{ "Gruvbox", { theme = "gruvbox", lua_loc = "lualine.themes.gruvbox" } },
+	{ "Everforest", { theme = "everforest", lua_loc = "lualine.themes.everforest" } },
+	{ "Blush", { theme = "everblush", lua_loc = "lualine.themes.everblush" } },
+	{ "Marakai", { theme = "marakai", lua_loc = "colorschemes.marakai_lualine" } },
+}
+
+-- create cmds from themes (colorscheme + lualine)
+local colo_command = require("colorschemes.set_colo_cmd")
+
+for _, theme in pairs(themes) do
+	pcall(colo_command, theme[1], theme[2]) -- (name, {colorscheme, lualine_folder})
+end
+
+-- Colorscheme Configs ------------------------------------
+
+-- Remove SignColumn background + carats in StatusLine
+vim.cmd([[
+  autocmd Colorscheme * highlight SignColumn guibg=NONE | highlight StatusLine gui=bold | highlight StatusLineNC gui=italic
+]])
+
+vim.o.background = "dark"
+
+-- Sonokai
+vim.cmd([[
+let g:sonokai_style = 'andromeda'
+let g:sonokai_better_performance = 1
+]])
+
+-- Tokyonight
+vim.g.tokyonight_style = "night"
+
+-- VSCode Dark
+local status, vscode = pcall(require, "vscode")
+if status then
+	vscode.setup({
+		transparent = false,
+	})
+end
+
+-- Startup Colorscheme ------------------------------------
+
+pcall(vim.cmd, ":Marakai")
---
+++ nvim/lua/lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+local status, configs = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
+
+configs.setup({
+	ensure_installed = "all",
+	ignore_install = { "" }, -- List of parsers to ignore installing
+	highlight = {
+		enable = true,
+		disable = { "" }, -- list of languages to disable
+	},
+	autopairs = {
+		enable = true,
+	},
+	indent = { enable = true, disable = { "python", "css" } },
+	-- Rainbow parentheses
+	rainbow = {
+		enable = true,
+		extended_mode = true, -- highlight non-bracket delimiters like html tags etc
+		colors = {
+			"#FFFF00",
+			"#3BAFF5",
+			"#ffd4fe",
+			"#bdfcd0",
+			"#a8f9ff",
+			"#c5c6fc",
+		},
+		max_file_lines = nil, -- Do not enable for files with more than n lines
+	},
+})
