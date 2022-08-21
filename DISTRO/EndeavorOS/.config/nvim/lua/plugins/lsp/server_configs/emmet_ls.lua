a43959ebdf119840369a4246577711307496f375
coffee-monk <coffeemonk@hotmail.com>
2022-08-21 08:34:28 -0400

    deleted tabnine model2

---
+++ DISTRO/EndeavorOS/.config/nvim/lua/plugins/lsp/server_configs/emmet_ls.lua
@@ -0,0 +1,11 @@
+return {
+	filetypes = { "html", "typescriptreact", "javascriptreact", "js", "javascript", "css", "sass", "scss", "less" },
+	init_options = {
+		html = {
+			options = {
+				-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+				["bem.enabled"] = true,
+			},
+		},
+	},
+}
